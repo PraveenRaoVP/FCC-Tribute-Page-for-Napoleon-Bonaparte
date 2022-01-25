5ab92d084b40734d96d1eac7a895cd86df668347
Praveen Rao V P <praveenraovp01@gmail.com>
2022-01-25 08:44:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+/*
+#Tribute Page tests
+passes:8 failures:2 duration:0.05
+#Technology Stack
+1. You can use HTML, JavaScript, and CSS to complete this project. Plain CSS is recommended because that is what the lessons have covered so far and you should get some practice with plain CSS. You can use Bootstrap or SASS if you choose. Additional technologies (just for example jQuery, React, Angular, or Vue) are not recommended for this project, and using them is at your own risk. Other projects will give you a chance to work with different technology stacks like React. We will accept and try to fix all issue reports that use the suggested technology stack for this project. Happy coding!
+o.assert.ok(!0)
+#Content
+1. My tribute page should have an element with corresponding id="main", which contains all other elements.
+2. I should see an element with corresponding id="title", which contains a string (i.e. text) that describes the subject of the tribute page (e.g. "Dr. Norman Borlaug").
+3. I should see either a <figure> or <div> element with corresponding id="img-div".
+4. Within the "img-div" element, I should see an <img> element with a corresponding id="image".
+5. Within the "img-div" element, I should see either a <figcaption> or <div> element with a corresponding id="img-caption" that contains textual content describing the image shown in "img-div".
+6. I should see an element with a corresponding id="tribute-info", which contains textual content describing the subject of the tribute page.
+
+Enter image centered along with parent
+
+Not done:-
+
+7. I should see an <a> element with a corresponding id="tribute-link", which links to an outside site that contains additional information about the subject of the tribute page. HINT: You must give your element an attribute of target and set it to "_blank" in order for your link to open in a new tab (i.e. target="_blank").
+expected null to not equal null
+AssertionError: expected null to not equal null
+    at Proxy.c (https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:549:1889)
+    at Proxy.u (https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:431:130)
+    at Function.a.isNotNull (https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:574:1549)
+    at a.<anonymous> (https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:608:101621)
+    at https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:593:259477
+    at ay.xg.run (https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:593:259772)
+    at r.Qg.runTest (https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:593:274680)
+    at https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:593:275616
+    at o (https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:593:274033)
+    at https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:593:274103
+#Layout
+1. The <img> element should responsively resize, relative to the width of its parent element, without exceeding its original size.
+Try using the "max-width" style property : expected 'none' to not equal 'none'
+AssertionError: Try using the "max-width" style property : expected 'none' to not equal 'none'
+    at Proxy.c (https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:549:1889)
+    at Proxy.u (https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:431:130)
+    at Function.a.notStrictEqual (https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:574:736)
+    at a.<anonymous> (https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:608:102282)
+    at https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:593:259477
+    at ay.xg.run (https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:593:259772)
+    at r.Qg.runTest (https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:593:274680)
+    at https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:593:275616
+    at o (https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:593:274033)
+    at https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js:593:274103
+
+
+*/
